4a0811da6b47a1980ef9fbe258788ac36d33d09a
markus <markusjsommer@gmail.com>
2024-07-31 10:55:19 -0400

    continue predict and remove top500

--- TD2/Predict.py
+++ TD2/Predict.py
@@ -12,13 +12,8 @@ import pandas
 import numpy as np
 from tqdm.auto import tqdm
 
-from multiprocessing import Pool
-
-from Bio import SeqIO
-from Bio.Seq import Seq
-
-from transmark.translator import Translator
-from transmark.LongOrfs import load_fasta
+from TD2.translator import Translator
+from TD2.LongOrfs import load_fasta
 
 
 def get_args():
@@ -29,16 +24,21 @@ def get_args():
     required.add_argument("-t", dest="transcripts",  type=str, required=True, help="REQUIRED path to transcripts.fasta")
     
     # optional
-    parser.add_argument("--retain_mmseqs_hits",  type=str, required=False, help="mmseqs output in default '.m8' format. Any ORF with a MMseqs2 match will be retained in the final output.")
-    parser.add_argument("--retain_blastp_hits",  type=str, required=False, help="blastp output in '-outfmt 6' format. Any ORF with a blast match will be retained in the final output.")
-    parser.add_argument("--retain_pfam_hits",  type=str, required=False, help="domain table output file from running hmmscan to search Pfam. Any ORF with a pfam domain hit will be retained in the final output.")
+    parser.add_argument("-P", dest="psauron_cutoff", type=float, required=False, help="minimum in-frame PSAURON score required to report ORF, assuming no homology hits (range: [0,1]; default: 0.25)", default=0.25)
+    parser.add_argument("--retain_mmseqs_hits",  type=str, required=False, help="mmseqs output in '.m8' format. Any ORF with a MMseqs2 match will be retained in the final output.")
+    parser.add_argument("--retain_blastp_hits",  type=str, required=False, help="blastp output in '-outfmt 6' format. Any ORF with a blastp match will be retained in the final output.")
+    parser.add_argument("--retain_hmmer_hits",  type=str, required=False, help="domain table output file from running hmmer to search Pfam. Any ORF with a Pfam domain hit will be retained in the final output.")
     
-    # TODO mmseqs for domain search. Hayden says she has something better?
+    # TODO allow for hits from multiple mmseqs databases?
     
-    parser.add_argument("--retain_long_orfs_length",  type=int, required=False, help="retain all ORFs found that are equal or longer than these many nucleotides even if no other evidence marks it as coding (default: 1000000) so essentially turned off by default.)", default=1000000)
+    parser.add_argument("--retain_long_orfs_length",  type=int, required=False, help="retain all ORFs found that are equal or longer than these many nucleotides even if no other evidence marks it as coding (default: 1000000, so essentially turned off by default.)", default=1000000)
     parser.add_argument("--single_best_only",  type=str, required=False, help="Retain only the single best orf per transcript (prioritized by homology then orf length)")
-    parser.add_argument("-O", dest="output_dir", type=str, required=False, help="same output directory from LongOrfs", default="./transcripts.transmark_dir")
+    parser.add_argument("-O", dest="output_dir", type=str, required=False, help="same output directory from LongOrfs", default="./transcripts.TD2_dir")
     parser.add_argument("-G", dest="genetic_code", type=int, required=False, help="genetic code a.k.a. translation table, NCBI integer codes, default=1", default=1)
+    
+    # TODO verbosity
+    parser.add_argument("-v", "--verbose", action='store_true', help="set -v for verbose output with progress bars, default=False", default=False)
+
 
     # TODO start codon refinement with PWM? Is there anything better?
 
@@ -51,68 +51,80 @@ def main():
     
     # parse command line arguments
     args = get_args()
+    psauron_cutoff = args.psauron_cutoff
     
     # use absolute path of output
     output_dir = os.path.abspath(args.output_dir)    
     
     # run psauron to score ORFs
+    print(f"Step 1: Running PSAURON", flush=True)
     p_cds = os.path.join(output_dir, "longest_orfs.cds")
     p_score = os.path.join(output_dir, "psauron_score.csv")
-    command_psauron = ["psauron", "-i"+str(p_cds), "-o" +str(p_score), "-m0"]
-    result_psauron = subprocess.run(command_psauron, capture_output=False, text=True)
+    if args.verbose:
+        command_psauron = ["psauron", "-i", str(p_cds), "-o", str(p_score), "-m", "0", "--inframe", str(psauron_cutoff), "-v"]
+        #result_psauron = subprocess.run(command_psauron, capture_output=False, text=True)
+    else:
+        command_psauron = ["psauron", "-i", str(p_cds), "-o", str(p_score), "-m", "0", "--inframe", str(psauron_cutoff)]
+        #result_psauron = subprocess.run(command_psauron, capture_output=True, text=True)
     
     # load psauron results
     df_psauron = pandas.read_csv(p_score, skiprows=3)
     
     # select transcripts based on psauron score
-    # in-frame > 0.5
-    # all out-of-frame < in-frame
-    df_psauron_selected = df_psauron[df_psauron.apply(lambda row: row['in_frame_score'] > 0.5 and all(row[3:] < row['in_frame_score']), axis=1)]
+    df_psauron_selected = df_psauron[df_psauron.apply(lambda row: row['in_frame_score'] > psauron_cutoff and all(row[3:] < row['in_frame_score']), axis=1)]
+    print(f"Done.")
     
-    ################### TEMP
-    df_psauron_selected.to_csv("~/desktop/foo.csv")
-    
-
     # integrate homology search results
-    # parse mmseqs 
+    if any([args.retain_mmseqs_hits, args.retain_blastp_hits, args.retain_hmmer_hits]):
+        print(f"Step 2: Integrating homology results", flush=True)
+    else:
+        print(f"Step 2: No homology search results provided, skipping", flush=True)
+    
+    # parse mmseqs
+    if args.retain_mmseqs_hits:
+        print(f"Loading MMseqs2 output", flush=True)
+        p_mmseqs = args.retain_mmseqs_hits
+        df_mmseqs = pandas.read_table(p_mmseqs, header=None)
+        hits_mmeseqs = set(df_mmseqs[0])
+        print(f"Found {int(len(hits_mmeseqs)):d} ORFs with MMseqs2 hits", flush=True)
     
     # parse blast
+    if args.retain_blastp_hits:
+        print(f"Loading blastp output", flush=True)
+        p_blastp = args.retain_blastp_hits
+        df_blastp = pandas.read_table(p_blastp, header=None)
+        hits_blastp = set(df_blastp[0])
+        print(f"Found {int(len(hits_blastp)):d} ORFs with blastp hits", flush=True)   
+        
+    # parse hmmer
+    if args.retain_hmmer_hits:
+        print(f"Loading hmmer output", flush=True)
+        p_hmmer = args.retain_hmmer_hits
     
-    # parse pfam
+        
     
     
     
-    # write intermediate output files to working directory
-    p_scores = os.path.join(output_dir, "longest_orfs.cds.scores")
-    with open(p_scores, "wt") as f:
-    # TODO
-        pass
-    p_scores_selected = os.path.join(output_dir, "longest_orfs.cds.scores.selected")
-    with open(p_scores_selected, "wt") as f:
-    # TODO
+    print(f"Done.")
+    
+    
+    # remove internal ORFs
+    
+    
+    # write final outputs to current working directory
+    p_pep = os.path.join(output_dir, "longest_orfs.pep")
+    p_gff3 = os.path.join(output_dir, "longest_orfs.gff3")
+    p_cds = os.path.join(output_dir, "longest_orfs.cds")
+    
+    basename = os.path.basename(args.transcripts)
+    p_pep_final = basename + ".TD2.pep"
+    p_gff3_final = basename + ".TD2.gff3"
+    p_cds_final = basename + ".TD2.cds"
+    p_bed_final = basename + ".TD2.bed" # TODO bed file, probably better just to write gff then convert
+    
+    with open(p_pep_final, "wt") as f_pep_final, open(p_gff3_final, "wt") as f_gff3_final, open(p_cds_final, "wt") as f_cds_final:
         pass
         
-    p_best_candidates = os.path.join(output_dir, "longest_orfs.cds.best_candidates.gff3")
-    with open(p_best_candidates, "wt") as f:
-    # TODO
-        pass
-    
-    
-    # write final outputs to current working directory 
-    # TODO does TransDecoder really just write to CWD regardless of -O? test this behavior
-    with open("transcripts.fasta.transdecoder.pep", "wt") as f:
-    # TODO
-        pass
-    with open("transcripts.fasta.transdecoder.cds", "wt") as f:
-    # TODO
-        pass
-    with open("transcripts.fasta.transdecoder.gff3", "wt") as f:
-    # TODO
-        pass
-    with open("transcripts.fasta.transdecoder.bed", "wt") as f:
-    # TODO
-        pass
-    
     
 if __name__ == "__main__":
     main()
