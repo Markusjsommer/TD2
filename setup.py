11cd6bbefea31addfe8f6f4938bd646cfcaf63fb
Markus <markus@Markuss-MacBook-Pro.local>
2026-01-08 09:58:13 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     
 setup(
     name="TD2",
-    version="1.0.6",
+    version="1.0.7",
     description="A tool to find protein coding ORFs",
     packages=find_packages(),
     long_description=long_description,
